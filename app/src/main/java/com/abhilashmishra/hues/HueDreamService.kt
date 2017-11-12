a753f724aaa6657c1e428984cfe1d0e08d5e32df
Abhilash Mishra <abhilash.mishra@swiggy.in>
2017-11-12 18:03:48 +0530

    added android tv support

--- app/src/main/java/com/abhilashmishra/hues/HueDreamService.kt
+++ app/src/main/java/com/abhilashmishra/hues/HueDreamService.kt
@@ -1,7 +1,14 @@
 package com.abhilashmishra.hues
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.service.dreams.DreamService
+import android.support.v7.graphics.Palette
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.abhilashmishra.hues.model.ListingResponse
@@ -9,7 +16,11 @@ import com.abhilashmishra.hues.model.PostData
 import com.abhilashmishra.hues.model.Subreddit
 import com.abhilashmishra.hues.network.Network
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -31,10 +42,24 @@ class HueDreamService : DreamService() {
     var lastShownImageIndex = -1
     var after: String? = null
     val subscriptions: CompositeDisposable = CompositeDisposable()
+    var lastBackgroundColor = Color.TRANSPARENT
 
     //Views
     lateinit var imageView: ImageView
     lateinit var authorName: TextView
+    lateinit var totalBackground: View
+
+    val paletteListener: Palette.PaletteAsyncListener = Palette.PaletteAsyncListener { palette ->
+        palette?.let {
+            val colorFrom: Int = lastBackgroundColor
+            val colorTo: Int = palette.getVibrantColor(Color.TRANSPARENT)
+            val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, colorTo);
+            colorAnimation.duration = 250; // milliseconds
+            colorAnimation.addUpdateListener { animator -> totalBackground.setBackgroundColor(animator.animatedValue as Int) }
+            colorAnimation.start();
+            lastBackgroundColor = colorTo
+        }
+    }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
@@ -48,6 +73,7 @@ class HueDreamService : DreamService() {
         setContentView(R.layout.hue_screen_layout)
         imageView = findViewById(R.id.image_view)
         authorName = findViewById(R.id.description)
+        totalBackground = findViewById(R.id.totalBackground)
         init()
     }
 
@@ -88,7 +114,7 @@ class HueDreamService : DreamService() {
                 .map { subreddit ->
                     subreddit.name?.let {
                         Network.getRedditApi(this)?.
-                                fetchPosts(subreddit.name, subreddit.after, 5)?.
+                                fetchPosts(subreddit.name, subreddit.after, null)?.
                                 map { listingResponse: ListingResponse? ->
                                     if (listingResponse != null) {
                                         subreddit.after = listingResponse.data.after
@@ -101,7 +127,7 @@ class HueDreamService : DreamService() {
     }
 
     private fun startImageView() {
-        subscriptions.add(Flowable.interval(15000, TimeUnit.MILLISECONDS)
+        subscriptions.add(Flowable.interval(20000, TimeUnit.MILLISECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSubscriber<Long>() {
@@ -113,12 +139,26 @@ class HueDreamService : DreamService() {
                         lastShownImageIndex++
                         if (lastShownImageIndex == imageList.size - 1) {
                             subscriptions.clear()
+                            lastShownImageIndex = 0
                             fetchImages()
                         }
 
                         Glide.with(applicationContext)
                                 .load(imageList[lastShownImageIndex].url)
-                                .transition(DrawableTransitionOptions.withCrossFade())
+                                .transition(DrawableTransitionOptions.withCrossFade(800))
+                                .listener(object : RequestListener<Drawable> {
+                                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                                        return false
+                                    }
+
+                                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                                        if (resource is BitmapDrawable) {
+                                            Palette.from(resource.bitmap).generate(paletteListener)
+                                        }
+                                        return false
+                                    }
+
+                                })
                                 .into(imageView)
 
                         authorName.text = "author: ${imageList[lastShownImageIndex].author}"
