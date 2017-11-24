4d29a62bcd890c28d91fc14014d2c43696542340
Abhilash Mishra <abhilash.mishra@swiggy.in>
2017-11-24 22:33:53 +0530

    error fix

---
+++ app/src/main/java/com/abhilashmishra/hues/ui/MainActivityPhone.kt
@@ -0,0 +1,13 @@
+package com.abhilashmishra.hues.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.abhilashmishra.hues.R
+
+class MainActivityPhone : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_phone)
+    }
+}
